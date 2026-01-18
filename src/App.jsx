c30ffd2aa131bf8617fadbecb4482aee72132236
taitoyama <taitoyamagoshi@gmail.com>
2026-01-18 11:40:35 +0900

    Appsを変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-//import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState("おおほり")
-
   return (
     <>
-      { }
-      {count}
-      { }
+      {/*  */}
+      <div>
+        <p>名前が入ります</p>
+        <input type="text" placeholder="名前を入力してください" />
+      </div>
+      <div>
+        <p>メールアドレスが入ります</p>
+        <input type="text" placeholder="メールアドレスを入力してください" />
+      </div>
+      {/*  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
