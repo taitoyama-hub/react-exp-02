570d618b5b498794e736e50ebc0621e101fbb739
taitoyama <taitoyamagoshi@gmail.com>
2026-01-18 12:19:23 +0900

    微調整

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,16 +6,39 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+
+  useEffect(() => {
+
+  }, [])
+
   return (
     <>
       {/*  */}
       <div>
         <p>名前が入ります</p>
-        <input type="text" placeholder="名前を入力してください" />
+        <input
+          type="text"
+          placeholder="名前を入力してください"
+          value={name}
+          onChange={handleNameChange}
+        />
       </div>
       <div>
         <p>メールアドレスが入ります</p>
-        <input type="text" placeholder="メールアドレスを入力してください" />
+        <input
+          type="text"
+          placeholder="メールアドレスを入力してください"
+          value={email}
+          onChange={handleEmailChange}
+        />
       </div>
       {/*  */}
     </>
