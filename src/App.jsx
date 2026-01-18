346fd10244f14f17d08b241e6c9b258ae1fa780b
taitoyama <taitoyamagoshi@gmail.com>
2026-01-18 13:20:51 +0900

    授業終わりの内容を反映

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import News from "./components/News";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-
-  useEffect(() => {
-    console.log("名前が変更されました");
-  }, [])
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
 
 
-  console.log("起動しました");
+    useEffect(() => {
+        console.log("名前が変更されました");
+    }, [])
 
-  return (
-    <>
-      {/*  */}
-      <div>
-        <p>名前が入ります</p>
-        <input
-          type="text"
-          placeholder="名前を入力してください"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </div>
-      <div>
-        <p>メールアドレスが入ります</p>
-        <input
-          type="text"
-          placeholder="メールアドレスを入力してください"
-          value={email}
-          onChange={handleEmailChange}
-        />
-      </div>
-      {/*  */}
-    </>
-  );
+
+    console.log("起動しました");
+
+    return (
+        <>
+            <News />
+            <div>
+                <p>名前が入ります</p>
+                <input
+                    type="text"
+                    placeholder="名前を入力してください"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </div>
+            <div>
+                <p>メールアドレスが入ります</p>
+                <input
+                    type="text"
+                    placeholder="メールアドレスを入力してください"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
