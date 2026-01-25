12dfeee46fa09cb397947a0dfe227630ff7ab73a
taitoyama <taitoyamagoshi@gmail.com>
2026-01-25 13:16:00 +0900

    0126の授業反映

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,20 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-import News from "./components/News";
-import SideBar from "./layout/SideBar";
-import Header from "./layout/Header";
-import ChartBar from "./components/ChartBar";
-import CalenderItem from "./components/CalenderItem";
-
+import Chart from "./pages/chart";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notfound";
 
 function App() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
-
-
-    useEffect(() => {
-        console.log("名前が変更されました");
-    }, [])
-
-
-    console.log("起動しました");
-
     return (
         <>
-            <Header />
-            <SideBar />
-            <News />
-            <ChartBar />
-            <CalenderItem />
-            <div>
-                <p>名前が入ります</p>
-                <input
-                    type="text"
-                    placeholder="名前を入力してください"
-                    value={name}
-                    onChange={handleNameChange}
-                />
-            </div>
-            <div>
-                <p>メールアドレスが入ります</p>
-                <input
-                    type="text"
-                    placeholder="メールアドレスを入力してください"
-                    value={email}
-                    onChange={handleEmailChange}
-                />
-            </div>
+            {/*  */}
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/chart" element={<Chart />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+            {/*  */}
         </>
     );
 }
