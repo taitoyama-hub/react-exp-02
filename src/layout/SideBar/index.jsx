22cfed48a0f92c9c105b349c34807dfab5bf8c1f
taitoyama <taitoyamagoshi@gmail.com>
2026-01-30 10:50:30 +0900

    サイドバー微修正

--- src/layout/SideBar/index.jsx
+++ src/layout/SideBar/index.jsx
@@ -17,14 +17,15 @@ const SideBar = () => {
                     <Link to="/customer">顧客リスト</Link>
                 </li>
                 <li>
+                    <Link to="/orders">注文管理</Link>
+                </li>
+                <li>
                     <Link to="/chart">チャート</Link>
                 </li>
                 <li>
                     <Link to="/logout">ログアウト</Link>
                 </li>
-                <li>
-                    <Link to="/orders">注文管理</Link>
-                </li>
+
             </ul>
             {/*  */}
         </div>
